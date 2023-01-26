5c49722584aee3c7409fc9e98c19ca7a4244d46a
Raman Tank <ramantank04022002@gmail.com>
2023-01-26 20:28:06 +0530

    Modified the Transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -15,45 +15,46 @@ class _TransactionListState extends State<TransactionList> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 300,
-        child: ListView.builder(
-          itemBuilder: (context, index) {
-            return Card(
-              elevation: 5,
-              margin: EdgeInsets.symmetric(vertical: 8,horizontal: 5),
-              
-              child: Row(
-                
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundColor: Colors.purple,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: FittedBox(
-                        child: Text(
-                          '\$${widget.transations[index].amount}',style: TextStyle(color: Colors.white,fontSize: 40),
-                        ),
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 5,
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.purple,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: FittedBox(
+                      child: Text(
+                        '\$${widget.transations[index].amount}',
+                        style: TextStyle(color: Colors.white, fontSize: 40),
                       ),
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(widget.transations[index].title,
-                          style: Theme.of(context).textTheme.titleLarge),
-                      Text(
-                        '   '+DateFormat.yMMMd()
-                            .format(widget.transations[index].date),
-                        style: TextStyle(color: Colors.grey.shade700),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            );
-          },
-          itemCount: widget.transations.length,
-        ));
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(widget.transations[index].title,
+                        style: Theme.of(context).textTheme.titleLarge),
+                    Text(
+                      '   ' +
+                          DateFormat.yMMMd()
+                              .format(widget.transations[index].date),
+                      style: TextStyle(color: Colors.grey.shade700),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: widget.transations.length,
+      ),
+    );
   }
 }
