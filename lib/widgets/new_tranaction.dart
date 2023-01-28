52eb68521dd7323fdd99eb0e183fa02ad6bb53ac
Raman Tank <ramantank04022002@gmail.com>
2023-01-28 19:08:41 +0530

    Added some button for IOS also but comment that

--- lib/widgets/new_tranaction.dart
+++ lib/widgets/new_tranaction.dart
@@ -1,5 +1,5 @@
-import 'dart:math';
-
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -92,6 +92,17 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
+                    // Platform.isIOS
+                    //     ? CupertinoButton(
+                    //         onPressed: _percentDatePicker,
+                    //         child: const Text(
+                    //           'Choose Date',
+                    //           style: TextStyle(
+                    //             fontWeight: FontWeight.bold,
+                    //           ),
+                    //         ),
+                    //       )
+                    //     :
                     TextButton(
                       onPressed: _percentDatePicker,
                       child: Text(
